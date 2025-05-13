f56dc7775576324adcf0a768c625af0588491401
ivin.s <ivin.s@codelynks.com>
2025-05-13 15:29:08 +0530

    02 comp

--- 02/test_add.py
+++ 02/test_add.py
@@ -16,6 +16,27 @@ class TestAddFunction(unittest.TestCase):
         self.assertEqual(add(0, 5), 5)
         self.assertEqual(add(5, 0), 5)
 
+    def test_add_with_string(self):
+        self.assertEqual(add("0", 5), None)
+
+
+class TestSubFunction(unittest.TestCase):
+    def test_add_positive_numbers(self):
+        self.assertEqual(add(2, 3), 5)
+
+    def test_add_negative_numbers(self):
+        self.assertEqual(add(-2, -3), -5)
+
+    def test_add_mixed_numbers(self):
+        self.assertEqual(add(-2, 3), 1)
+
+    def test_add_with_zero(self):
+        self.assertEqual(add(0, 5), 5)
+        self.assertEqual(add(5, 0), 5)
+
+    def test_add_with_string(self):
+        self.assertEqual(add("0", 5), None)
+
 
 if __name__ == "__main__":
     unittest.main()
