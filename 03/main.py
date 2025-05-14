11f287e1fc0021b029db48a4901882f02380540e
ivin.s <ivin.s@codelynks.com>
2025-05-14 09:24:40 +0530

    update things

--- 03/main.py
+++ 03/main.py
@@ -1,8 +1,12 @@
 def add(a, b):
     if not isinstance(a, int):
-        return None
+        raise ValueError("a needs to be a int")
     return a + b
 
 
 def sub(a, b):
     return a - b
+
+
+def div(a, b):
+    return a / b
--- 03/tests/test_add.py
+++ 03/tests/test_add.py
@@ -1,26 +1,32 @@
-from main import add, sub
+import pytest
+from main import add, sub, div
 
 
-def test_add_positive_numbers():
-    assert add(2, 3) == 5
+class TestAdd:
+    def test_add_positive_numbers(self):
+        assert add(2, 3) == 5
+
+    def test_add_negative_numbers(self):
+        assert add(-2, -3) == -5
+
+    def test_add_mixed_numbers(self):
+        assert add(-2, 3) == 1
+
+    def test_add_with_zero(self):
+        assert add(0, 5) == 5
+        assert add(5, 0) == 5
+
+    def test_add_with_string(self):
+        with pytest.raises(ValueError):
+            add("0", 5)
 
 
-def test_add_negative_numbers():
-    assert add(-2, -3) == -5
+# def test_sub_testing():
+#     assert sub(1, 2) == -1
 
 
-def test_add_mixed_numbers():
-    assert add(-2, 3) == 1
+# def test_div_by_zero():
+#     with pytest.raises(ZeroDivisionError):
+#         div(1, 0)
 
-
-def test_add_with_zero():
-    assert add(0, 5) == 5
-    assert add(5, 0) == 5
-
-
-def test_add_with_string():
-    assert add("0", 5) is None
-
-
-def test_sub_testing():
-    assert sub(1, 2) == -1
+#     assert div(0, 1) == 0
